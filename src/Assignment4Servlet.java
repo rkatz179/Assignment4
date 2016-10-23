96f3a788f38a954ca1725bfb7761c150e2530933
Rachel Katz <rkatz179@gmail.com>
2016-10-23 17:43:03 -0400

    Fixing minor bugs and making it ~pretty~

--- src/Assignment4Servlet.java
+++ src/Assignment4Servlet.java
@@ -1,25 +1,51 @@
+/**
+* Authors: Rachel Katz (rak5ea), Emma Fass (enf5cb), Summer Thompson (sst2bd) 
+*/
+
+/**
+* Import all necessary libraries
+*/
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 
+/**
+* Define Assignment4Servlet to extend HTTPServlet
+*/
 public class Assignment4Servlet extends HttpServlet {
 
+	/**
+	* Define the page link
+	*/
 	private static String Assignment4Servlet = "http://localhost:8080/Assignment4/Assignment4Servlet";
 
+	/**
+	* Declare all buttons
+	*/
 	private static String submitBtn;
 	private static String convincedBtn;
 	private static String disagreeBtn;
 	private static String unsureBtn;
 	
+	/**
+	*  Declare counter arguments
+	*/
    	private int convinced = 0;
    	private int disgree = 0;
    	private int unsure = 0;
 
+   	/**
+	* Declare Claim and Argument fields
+	*/
    	private String claimField;
    	private String argumentField;
 
 
+   	/**
+	*  Method Name: doGet()
+	*  Summary: This is called when the page is loaded and when data is being sent to the server
+	*/
     public void doGet(HttpServletRequest request,
 		      HttpServletResponse response)
 	throws ServletException, IOException {
@@ -38,7 +64,8 @@ public class Assignment4Servlet extends HttpServlet {
 	out.println("</HEAD>");
 	out.println("<BODY>");
 	out.println("<h1>CS 4501 Assignment-4</h1> ");
-	out.println("<p>Directions: </br></br> In the text below, you will begin by making a claim or assertion, and then in the next box you will explain why you have made this claim.</p>");
+	out.println("<center>");
+	out.println("<p>Directions: </br></br> In the text below, you will begin by making a claim or assertion,</br> and then in the next box you will explain why you have made this claim.</p>");
 	out.println("<form action="+ Assignment4Servlet + " method=\"post\" name=\"info\" >");	
 	out.println("<strong> Step 1 </strong> - Assert Something");
 	out.println("<textarea name=\"Claim\" placeholder=\"Claim/Assertion:\" rows=\"10\" cols=\"50\">");
@@ -46,50 +73,44 @@ public class Assignment4Servlet extends HttpServlet {
   	out.println("<strong> Step 2 </strong> - Now <i>prove</i> it");
   	out.println("<textarea name=\"Argument\" placeholder=\"Argument/Evidence:\" rows=\"10\" cols=\"50\">");
   	out.println("</textarea></br></br>");
-  	out.println("<input type=\"submit\" name=\"Submit\" value=\"Submit\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
+  	out.println("<input type=\"submit\" name=\"Submit\" class=\"counter\" value=\"Submit\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
 	out.println("</form><br/>");
 	out.println("<form action="+ Assignment4Servlet + " method=\"post\" name=\"buttons\" >");	
-	out.println("<center>");
 	out.println("<table cellpadding=5 cellspacing=2 >");
 	out.println("<tr>");
 	out.println("<td>Vote here: <input type=\"submit\" name=\"cBtn\" value=\"Convinced\"> <input type=\"submit\" name=\"dBtn\" value=\"Disagree\"><input type=\"submit\" name=\"uBtn\" value=\"Unsure\"></td>");
 	out.println("</tr>");
 	out.println("</table>");
 	out.println("</form><br/>");
-	out.println("<p>Convinced Button = " + convincedBtn + " </p>");
 	out.println("<p>Convinced= " + convinced + " </p>");
 	out.println("<p>Disagree= " + disgree + " </p>");
 	out.println("<p>Unsure= " + unsure + " </p>");
-	out.println("</center>");
-	out.println("<center>");
-	out.println("<form name=\"choice\">");
-	out.println("<h2>Choose your favorite ice cream flavor</h5>");
-	out.println("<img src=\"https://media1.giphy.com/media/bDcs1vQmKdsqc/200_s.gif\">");
-	out.println("<input type=\"submit\" class=\"flavor\" value=\"Vanilla\">");
-	out.println("<input type=\"submit\" class=\"flavor\" value=\"Chocolate\">");
-	out.println("<input type=\"submit\" class=\"flavor\" value=\"Strawberry\">");
-	out.println("</form>");
-	out.println("</center>");
-	out.println("<center>");
-	out.println("<h2>Click the buttons</h5>");
-	out.println("<input type=\"submit\" class=\"counter\" value=\"Click Me\">");
-	out.println("<input type=\"submit\" class=\"counter\" value=\"Reset\">");
-	out.println("<p>The button has been clicked <p id='number'>0</p> times</p>");
 	out.println("</center>");
 	out.println("</BODY>");
 	out.println("</HTML>");
     }
 
+    /**
+	*  Method Name: doPost()
+	*  Summary: This is called when information is received by the server, 
+	*  		     and new data must be displayed on the page
+	*/
     public void doPost(HttpServletRequest request,
 		      HttpServletResponse response)
 	throws ServletException, IOException {
 		
+			/**
+			*  Specify that HTML will be used and initialize the PrintWriter
+			*/
 			response.setContentType("text/html");
 			PrintWriter out = response.getWriter();
 
-			log("I made it to Post");
-
-			submitBtn = request.getParameter("Submit");
+			/**
+			*  Get parameters and check for null or empty fields
+			*/
+			if(submitBtn == null || submitBtn == ""){
+				submitBtn = request.getParameter("Submit");
+			}
 
 			convincedBtn = request.getParameter("cBtn");
 			if(convincedBtn != null && convincedBtn.equals("Convinced")){
@@ -114,6 +135,10 @@ public class Assignment4Servlet extends HttpServlet {
 				claimField = request.getParameter("Claim");
 			}
 
+
+			/**
+			*  Changed HTML to be displayed after submission of forms
+			*/
 			out.println("<HTML>");
 			out.println("<HEAD>");
 			out.println("<meta http-equiv=\"content-type\" content=\"text/html; charset=windows-1252\">");
@@ -125,9 +150,15 @@ public class Assignment4Servlet extends HttpServlet {
 			out.println("</HEAD>");
 			out.println("<BODY>");
 			out.println("<h1>CS 4501 Assignment-4</h1> ");
-			out.println("<p>Directions: </br></br> In the text below, you will begin by making a claim or assertion, and then in the next box you will explain why you have made this claim.</p>");
+			out.println("<center>");
+			out.println("<p>Directions: </br></br> In the text below, you will begin by making a claim or assertion, <br/>and then in the next box you will explain why you have made this claim.</p>");
 			out.println("<form action="+ Assignment4Servlet + " method=\"post\" name=\"info\" >");				
 			out.println("<strong> Step 1 </strong> - Assert Something");
+
+			/**
+			*  If the claimfield is not null or empty, display the claim inputed by the user.
+			*  Else, display an error as well as the original textfield
+			*/
 			if(claimField != "" && claimField != null){ 
 				out.println("<p> Claim: " + claimField + " </p>"); 
 			}
@@ -137,6 +168,11 @@ public class Assignment4Servlet extends HttpServlet {
 				out.println("</textarea></br></br>");
 			}
 		  	out.println("<strong> Step 2 </strong> - Now <i>prove</i> it");
+		  	
+		  	/**
+			*  If the argument is not null or empty, display the claim inputed by the user.
+			*  Else, display an error as well as the original textfield
+			*/
 		  	if(argumentField != "" && argumentField != null){ 
 				out.println("<p> Argument: " + argumentField + " </p>"); 
 			}
@@ -145,40 +181,30 @@ public class Assignment4Servlet extends HttpServlet {
 				out.println("<textarea name=\"Argument\" placeholder=\"Argument/Evidence:\" rows=\"10\" cols=\"50\">");
 				out.println("</textarea></br></br>");
 			}
+
+			/**
+			*  Disable the submit button if both the claim and argument were submitted
+			*/
 			if(claimField != "" && argumentField != "" && submitBtn != null && submitBtn.equals("Submit")){
-				out.println("<input type=\"submit\" disabled=\"disabled\" name=\"Submit\" value=\"Submit\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
+				out.println("<input type=\"submit\" class=\"counter\" style=\"background-color:grey; opacity:0.40\" disabled=\"disabled\" name=\"Submit\" value=\"Submit\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
 			}
 			else{
-				out.println("<input type=\"submit\" name=\"Submit\" value=\"Submit\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
+				out.println("<input type=\"submit\" class=\"counter\" name=\"Submit\" value=\"Submit\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
 			}
 			out.println("</form><br/>");
 			out.println("<form action="+ Assignment4Servlet + " method=\"post\" name=\"buttons\" >");	
-			out.println("<center>");
 			out.println("<table cellpadding=5 cellspacing=2 >");
 			out.println("<tr>");
-			out.println("<td>Vote here: <input type=\"submit\" name=\"cBtn\" value=\"Convinced\" action="+ Assignment4Servlet + " method=\"post\"> <input type=\"submit\" name=\"dBtn\" value=\"Disagree\"><input type=\"submit\" name=\"uBtn\" value=\"Unsure\"></td>");
+			out.println("<td>Vote here: <input type=\"submit\" name=\"cBtn\" value=\"Convinced\" action="+ Assignment4Servlet + " method=\"post\"> <input type=\"submit\" name=\"dBtn\" value=\"Disagree\"><input type=\"submit\"  name=\"uBtn\" value=\"Unsure\"></td>");
 			out.println("</tr>");
 			out.println("</table>");
 			out.println("</form><br/>");
-			out.println("<p>Convinced Button = " + convincedBtn + " </p>");
+			/**
+			*  Update the counts of the voting fields
+			*/
 			out.println("<p>Convinced= " + convinced + " </p>");
 			out.println("<p>Disagree= " + disgree + " </p>");
 			out.println("<p>Unsure= " + unsure + " </p>");
-			out.println("</center>");
-			out.println("<center>");
-			out.println("<form name=\"choice\">");
-			out.println("<h2>Choose your favorite ice cream flavor</h5>");
-			out.println("<img src=\"https://media1.giphy.com/media/bDcs1vQmKdsqc/200_s.gif\">");
-			out.println("<input type=\"submit\" class=\"flavor\" value=\"Vanilla\">");
-			out.println("<input type=\"submit\" class=\"flavor\" value=\"Chocolate\">");
-			out.println("<input type=\"submit\" class=\"flavor\" value=\"Strawberry\">");
-			out.println("</form>");
-			out.println("</center>");
-			out.println("<center>");
-			out.println("<h2>Click the buttons</h5>");
-			out.println("<input type=\"submit\" class=\"counter\" value=\"Click Me\">");
-			out.println("<input type=\"submit\" class=\"counter\" value=\"Reset\">");
-			out.println("<p>The button has been clicked <p id='number'>0</p> times</p>");
 			out.println("</center>");
 			out.println("</BODY>");
 			out.println("</HTML>");
